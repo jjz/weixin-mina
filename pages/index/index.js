4b58d6ddb7b1ece1c513656355a158fdea6d623d
jjz <woaf1003@gmail.com>
2016-10-25 13:30:20 +0800

    upgrade icon

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,7 @@
 //index.js
 //获取应用实例
 var app = getApp()
-Page({
+Page( {
   data: {
     motto: 'Hello World',
     userInfo: {}
@@ -12,19 +12,24 @@ Page({
   //     url: '../logs/logs'
   //   })
   // },
-  onFlexClick:function(){
-    wx.navigateTo({
-      url:'../flex/flex'
+  onFlexClick: function() {
+    wx.navigateTo( {
+      url: '../flex/flex'
     })
   },
-  onLoad: function () {
-    console.log('onLoad')
+  onIconClick: function() {
+    wx.navigateTo( {
+      url:'../icon/icon'
+    })
+  },
+  onLoad: function() {
+    console.log( 'onLoad' )
     var that = this
     //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
+    app.getUserInfo( function( userInfo ) {
       //更新数据
-      that.setData({
-        userInfo:userInfo
+      that.setData( {
+        userInfo: userInfo
       })
     })
   }
